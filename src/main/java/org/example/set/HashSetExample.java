b7be6fd4375b114288adc063ff8164df4038439c
Milena Bilewska <bilewskamilena@gmail.com>
2023-10-28 10:41:29 +0200

    Aktualny

--- src/main/java/org/example/set/HashSetExample.java
+++ src/main/java/org/example/set/HashSetExample.java
@@ -7,40 +7,40 @@ public class HashSetExample {
 
     public static void main(String[] args) {
 
-//        // Tworzenie HashSet przechowującego liczby całkowite
-//        Set<Integer> mojHashSet = new HashSet<>();
-//
-//        // Dodawanie elementów do HashSet
-//        mojHashSet.add(1);
-//        mojHashSet.add(2);
-//        mojHashSet.add(3);
-//
-//        System.out.println(mojHashSet);
-//
-//        System.out.println("  ");
-//
-//        // Dodawanie duplikatu - zostanie zignorowany
-//        mojHashSet.add(2);
-//
-//        // Rozmiar HashSet
-//        int rozmiarHashSet = mojHashSet.size();
-//        System.out.println("Rozmiar HashSet: " + rozmiarHashSet);
-//
-//
-//
-//        // Sprawdzanie, czy HashSet zawiera konkretny element
-//        boolean czyZawiera = mojHashSet.contains(3);
-//        System.out.println("Czy HashSet zawiera 3? " + czyZawiera);
-//
-//        // Usuwanie elementu z HashSet
-//        mojHashSet.remove(2);
-//
-//        // Iterowanie przez HashSet i wyświetlanie jego zawartości
-//        System.out.println("Zawartość HashSet:");
-//        for (Integer liczba : mojHashSet) {
-//            System.out.println(liczba);
-//        }
-//
+        // Tworzenie HashSet przechowującego liczby całkowite
+        Set<Integer> mojHashSet = new HashSet<>();
+
+        // Dodawanie elementów do HashSet
+        mojHashSet.add(1);
+        mojHashSet.add(2);
+        mojHashSet.add(3);
+
+        System.out.println(mojHashSet);
+
+        System.out.println("  ");
+
+        // Dodawanie duplikatu - zostanie zignorowany
+        mojHashSet.add(2);
+
+        // Rozmiar HashSet
+        int rozmiarHashSet = mojHashSet.size();
+        System.out.println("Rozmiar HashSet: " + rozmiarHashSet);
+
+
+
+        // Sprawdzanie, czy HashSet zawiera konkretny element
+        boolean czyZawiera = mojHashSet.contains(3);
+        System.out.println("Czy HashSet zawiera 3? " + czyZawiera);
+
+        // Usuwanie elementu z HashSet
+        mojHashSet.remove(2);
+
+        // Iterowanie przez HashSet i wyświetlanie jego zawartości
+        System.out.println("Zawartość HashSet:");
+        for (Integer liczba : mojHashSet) {
+            System.out.println(liczba);
+        }
+
 
 
         HashSet<String> cars = new HashSet<String>();
@@ -62,6 +62,8 @@ public class HashSetExample {
         for (String i : cars) {
             System.out.println(i);
         }
+
+
 
 
     }
