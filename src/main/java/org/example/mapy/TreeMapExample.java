b77182bb766969eed704231040d7d2ef3e55b9e6
Milena Bilewska <bilewskamilena@gmail.com>
2023-10-28 11:28:20 +0200

    Aktualny 11.28

--- src/main/java/org/example/mapy/TreeMapExample.java
+++ src/main/java/org/example/mapy/TreeMapExample.java
@@ -9,10 +9,34 @@ public class TreeMapExample {
         // Tworzenie TreeMap przechowującej pary klucz-wartość typu String-Integer
         Map<String, Integer> mojaMapa = new TreeMap<>();
 
+        // Tworzenie TreeMap przechowującej pary klucz-wartość typu String-String
+        Map<String, String> mojaMapaString = new TreeMap<>();
+        mojaMapaString.put("Klucz 1", "Wartosc 1");
+        mojaMapaString.put("Klucz 2", "Wartosc 2");
+        mojaMapaString.put("Klucz 3", "Wartosc 444");
+        mojaMapaString.put("Klucz 4", "Wartosc 2");
+
+        System.out.println(mojaMapaString );
+
+        System.out.println("    ");
+
+        // Tworzenie TreeMap przechowującej pary klucz-wartość typu Integer-Double
+        Map<Integer, Double> mojaMapaIntegerDouble = new TreeMap<>();
+        mojaMapaIntegerDouble.put(1,2.22);
+        mojaMapaIntegerDouble.put(2,55.5);
+        System.out.println(mojaMapaIntegerDouble);
+
+        System.out.println("    ");
+
         // Dodawanie par klucz-wartość do TreeMap
         mojaMapa.put("Klucz 3", 300);
         mojaMapa.put("Klucz 1", 100);
         mojaMapa.put("Klucz 2", 200);
+
+
+
+
+
 
         // Pobieranie wartości na podstawie klucza
         int wartosc = mojaMapa.get("Klucz 2");
