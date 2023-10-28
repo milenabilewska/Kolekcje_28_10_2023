8e50dc60e5492b547c19994f1968322fdcbc156d
Milena Bilewska <bilewskamilena@gmail.com>
2023-10-28 10:41:54 +0200

    Aktualny

--- src/main/java/org/example/set/HashSetExample.java
+++ src/main/java/org/example/set/HashSetExample.java
@@ -47,7 +47,6 @@ public class HashSetExample {
         cars.add("Volvo");
         cars.add("BMW");
         cars.add("Ford");
-        cars.add("BMW");
         cars.add("Mazda");
         System.out.println(cars);
 
